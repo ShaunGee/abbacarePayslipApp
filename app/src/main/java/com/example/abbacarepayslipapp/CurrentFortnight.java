3c9652bddfc91fda51fb0ef26773b58432b46821
shaun <shaungumbaketi@gmail.com>
2020-09-30 16:13:24 +1000

    Initial Commit. Includes outlines of all 4 activities. No programming done done yet.

---
+++ app/src/main/java/com/example/abbacarepayslipapp/CurrentFortnight.java
@@ -0,0 +1,14 @@
+package com.example.abbacarepayslipapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CurrentFortnight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_current_fortnight);
+    }
+}
---
+++ app/src/main/java/com/example/abbacarepayslipapp/ReviewShift.java
@@ -0,0 +1,14 @@
+package com.example.abbacarepayslipapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReviewShift extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_review_shift);
+    }
+}
